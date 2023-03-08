eb287be96f6f299871385c30948c6493feb3cbf5
Seydi Sarıtaş <saritas.seydi@gmail.com>
2023-03-08 16:42:15 +0300

    project created

---
+++ fe/src/components/ExampleForm.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const ExampleForm = () => {
+    return (
+        <Box>
+            dasda
+        </Box>
+    )
+}
+
+export default ExampleForm
